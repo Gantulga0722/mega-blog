6a978696f18523ca72d9571f31220e454fbc2cd6
Gantulga0722 <mgtulga22@gmail.com>
2024-02-01 13:00:42 +0800

    From Pinecone

--- src/utils/dummyDataHL.js
+++
@@ -1,9 +0,0 @@
-import { HighlightImg } from "@/components/icons";
-export const highlightData = [
-  {
-    badge: "Technology",
-    title: "Grid system for better Design User Interface",
-    date: "August 20, 2022",
-    image: "/images/highlight-img.jpg",
-  },
-];
